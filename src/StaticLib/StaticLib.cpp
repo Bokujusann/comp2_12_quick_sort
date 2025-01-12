84069a440de8aa4bdf2b93c25e19d2c0967dd520
MankiYusuke <takosumi1271@ymail.ne.jp>
2025-01-12 20:32:07 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -3,10 +3,55 @@
 
 #include "../include/lib_func.h"
 
-// クイックソート(引数が不適切であればfalseを返す)
+void swap(item* a, item* b)
+{
+    item temp = *a;
+    *a = *b;
+    *b = temp;
+
+}
+
+item* partition(item* low, item* high)
+{
+    int a = high->key;
+    item* i = low - 1;
+
+    for (item* j = low; j < high; ++j)
+    {
+        if (j->key <= a)
+        {
+            ++i;
+            swap(i, j);
+
+
+            /**/
+        }
+    }
+    swap(i + 1, high);
+    return i + 1;
+}
+
+void quickSortRecursive(item* low, item* high)
+{
+    if (low < high)
+    {
+
+        item* pi = partition(low, high);
+
+        quickSortRecursive(low, pi - 1);
+        quickSortRecursive(pi + 1, high);
+    }
+}
+
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
+    
+    if (begin == NULL || end == NULL || begin >= end)
+    {
+        return false; // 無効な引数の場合は false を返す
+    }
 
-	return false;
+    quickSortRecursive(begin, (item*)(end - 1));
+    return true;
 }
